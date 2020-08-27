fe70ee687e1e6a3aef8bb562e2dd33866cff82a4
GuilhermeColturato <guilhermecolturato13@gmail.com>
2020-08-26 21:48:37 -0300

    Arquivo de interface desenvolvido

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Sequência de Fibonacci</title>
+	</head>
+	
+	<body>
+		<form action="processa.php" method="GET">
+			<label>Digite um numero:</label>
+			<input type="number" name="termo">
+			
+			<input type="submit" value="Enviar"/>
+		</form>
+	</body>
 </html>
