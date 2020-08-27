56d19f861846dc3cf82843f2210bc73ef073c920
GuilhermeColturato <guilhermecolturato13@gmail.com>
2020-08-26 21:50:21 -0300

    Arquivo de calculo codificado

--- processa.php
+++ processa.php
@@ -1,3 +1,15 @@
 <?php
-
+	$n = $_GET["termo"];
+	$a = 0;
+	$b = 1;
+	
+	echo "Sequência de Fibonacci do termo 1 ao termo $n é: </br>";
+	
+	echo "0 <br/> 1 <br/>";
+	
+	for($i=1; $i<=$n-2; $i++){
+		$b = $a + $b;
+		$a = $b - $a;
+		echo $b. "<br/>";
+	}
 ?>
